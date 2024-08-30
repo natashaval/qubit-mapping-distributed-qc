fac7c6ba6ccd94e0b9c02ac51da79bf66e8e3852
natashaval <natasha.valentina1998@gmail.com>
2024-08-30 17:40:12 +0100

    add comment from 0 to 4

--- lib/distributed_coupling_map.py
+++ lib/distributed_coupling_map.py
@@ -1,8 +1,11 @@
 from qiskit.transpiler import CouplingMap
 from qiskit_ibm_runtime.fake_provider import FakeLondonV2
 
+"""
+From documentation: https://docs.quantum.ibm.com/api/qiskit/qiskit.transpiler.CouplingMap
+"""
 
-# create ring
+# coupling list ring: Return a coupling map of n qubits connected to each of their neighbors in a ring.
 def build_coupling_list_ring(num_qubits: int, num_group: int):
     coupling_list = []
     for i in range(num_group):
@@ -18,6 +21,7 @@ def build_coupling_list_ring(num_qubits: int, num_group: int):
         coupling_list.append((i * num_qubits - 1, i * num_qubits))
     return coupling_list
 
+# coupling list full: Return a fully connected coupling map on n qubits.
 def build_coupling_list_full(num_qubits: int, num_group: int):
     coupling_list = []
     for i in range(num_group):
@@ -33,12 +37,12 @@ def build_coupling_list_full(num_qubits: int, num_group: int):
         coupling_list.append((i * num_qubits - 1, i * num_qubits))
     return coupling_list
 
-# coupling list line
+# coupling list line: Return a coupling map of n qubits connected in a line.
 def build_coupling_list_line(num_qubits: int, num_group: int):
     cm = CouplingMap.from_line(num_qubits=num_qubits * num_group, bidirectional=True)
     return cm.get_edges()
 
-# coupling list square
+# coupling list grid: Return a coupling map of qubits connected on a grid of num_rows x num_columns.
 def build_coupling_list_grid(num_rows: int, num_columns: int, num_group: int):
     coupling_list = []
     multiplier = num_rows * num_columns
@@ -55,7 +59,7 @@ def build_coupling_list_grid(num_rows: int, num_columns: int, num_group: int):
         coupling_list.append((i * multiplier - 1, i * multiplier))
     return coupling_list
 
-
+# coupling list t_horizontal: Return a coupling map of 5 T-shaped qubits connected on longer end.
 def build_coupling_list_t_horizontal(
     num_group: int,
 ):  # num qubits = 5, horizontal connect with 4
@@ -75,7 +79,7 @@ def build_coupling_list_t_horizontal(
         coupling_list.append((i * num_qubits - 1, i * num_qubits))
     return coupling_list
 
-
+# coupling list t_vertical: Return a coupling map of 5 T-shaped qubits connected on shorter end.
 def build_coupling_list_t_vertical(num_group: int):
     coupling_list = []
     fake_london = FakeLondonV2()
